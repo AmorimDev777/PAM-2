98be097274a36fd8867741d130db394361cdbfc8
Amorim_gk.12 <gabriel.amorim.etec@gmail.com>
2026-03-10 15:06:48 +0000

    Inicio APP

--- APP/App.js
+++ APP/App.js
@@ -1,33 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AppRoutes from './routes/app.routes'
 
 export default function App() {
-  var requestOptions = {
-   
-    method: 'GET',
-    redirect: 'follow'
-  };
-
-  fetch("http://localhost:3000/posts", requestOptions)
-  .then(response => response.json())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello Mundo!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <AppRoutes />
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundImage: 'linear-gradient(to bottom, white 60%, black 100%)',
+    backgroundImage: 'linear-gradient(to bottom, white, #414141)',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
     fontSize: 30
   }
-});
+})
